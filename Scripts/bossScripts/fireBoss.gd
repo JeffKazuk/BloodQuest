da79fdb56ec98cb30f95ce4727bc6aee7497a292
Jeff Kazuk <jeffreykazuk@gmail.com>
2019-04-27 11:50:32 -0400

    wew

--- Scripts/bossScripts/fireBoss.gd
+++ Scripts/bossScripts/fireBoss.gd
@@ -2,42 +2,69 @@ extends KinematicBody2D
 
 
 var speed = 2
-export (Vector2) var dir
+export (Vector2) var direction
 var timer = 5
 var angle_to_player = 0;
 signal fire (Fire, rotation, position)
+var player
 
 func _ready():
 	set_physics_process(true)
+	direction = Vector2()
+	player = get_parent().find_node("Player")
 	#print(target)
 
 func _physics_process(delta):
 	var target = get_parent().find_node("Player")
-	dir = (target.global_position - global_position).normalized()
+	direction = (target.global_position - global_position).normalized()
 	var distance = global_position.distance_to(target.global_position)
-	set_global_rotation(current_angle())
+	#$set_global_rotation(deg2rad(current_angle()))
 	#print(distance)
 	if distance > 100:
-		move_and_collide(dir*speed)
+		move_and_collide(direction*speed)
 	if distance < 100: #&& distance > 75:
-		move_and_collide(-dir*(speed*1.5))  
+		move_and_collide(-direction*(speed*1.5))  
 
 func _process(delta):
 	timer -= delta
 	if timer<=0:
 		attack()
 		timer = 5
+	print(rotation)
+	var facing = 'E'
+	var angle = direction.angle()
+	print(angle)
+	if angle > PI/8 && angle < 3*PI/8:
+		facing = 'SE'
+	if angle > 3*PI/8 && angle < 5*PI/8:
+		facing = 'S'
+	if angle > 5*PI/8 && angle < 7*PI/8:
+		facing = 'SW'
+	if angle > 7*PI/8 && angle < PI:
+		facing = 'W'
+	if angle > -PI/8 && angle < PI/8:
+		facing = 'E'
+	if angle > -3*PI/8 && angle < -PI/8:
+		facing = 'NE'
+	if angle > -5*PI/8 && angle < -3*PI/8:
+		facing = 'N'
+	if angle > -7*PI/8 && angle < -5*PI/8:
+		facing = 'NW'
+	if angle > -PI && angle < -7*PI/8:
+		facing = 'W'
+
+	$AnimatedSprite.animation = facing
 
 func current_angle():
-	var player = get_parent().find_node("Player")
-	angle_to_player = rad2deg(dir.angle_to(player.direction))
+	angle_to_player = rad2deg(position.angle_to_point(player.position))
+	#print(angle_to_player)
 	return angle_to_player
 
 func attack():
     print("Enemy is attacking")
     var player = get_parent().find_node("Player")
     var Fire = preload('res://Scenes/Player/Fire.tscn')
-    emit_signal('fire', Fire, current_angle(), position)
+    emit_signal('fire', Fire, direction.angle(), position)
     if position.distance_to(player.global_position) < 70:
         #direction = (player.global_position - global_position).normalized()
         
