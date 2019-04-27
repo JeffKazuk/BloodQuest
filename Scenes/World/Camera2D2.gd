7f3ab5fdbc6de957efe0e0581e313b2dd539f0cc
Shane <shanescott86@gmail.com>
2019-04-27 15:46:44 -0500

    test

---
+++ Scenes/World/Camera2D2.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var target = "Player"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    target = get_parent().find_node(target)   
+    position = target.position
+	
+func _physics_process(delta):
+    position = target.position
