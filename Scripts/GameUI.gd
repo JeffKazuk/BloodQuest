f63ce44f5da851a4d04ee030ebb818680f8f209f
Jeff Kazuk <jeffreykazuk@gmail.com>
2019-04-27 15:09:31 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Scripts/GameUI.gd
@@ -0,0 +1,5 @@
+extends Control
+signal health_changed(health)
+
+func _on_Health_health_changed(health):
+	emit_signal('health_changed', health)
---
+++ Scripts/Healthbar.gd
@@ -0,0 +1,6 @@
+extends TextureProgress
+
+
+
+func _on_Gameplay_UI_health_changed(health):
+	value = health
