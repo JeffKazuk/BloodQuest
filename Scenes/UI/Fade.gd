5d0648bc1e0d1a14369736699dbb4c4330db3cbe
Shane <shanescott86@gmail.com>
2019-04-28 13:49:25 -0500

    aded main menu

--- Scenes/UI/Fade.gd
+++ Scenes/UI/Fade.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Control
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,12 +9,14 @@ func _ready():
 	get_tree().paused = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var timer = 2
+	if(timer<=0):
+		get_parent().remove_child(self)
+	timer -= delta
 
 
 func _on_TextureButton_pressed():
 	get_tree().paused = false
-	#self.scale = Vector2(0, 0) # Hide
+	self.visible = false
 	
-
