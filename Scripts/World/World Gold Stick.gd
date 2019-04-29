dfb189a58c6ef141dcae5adee54d15dd3bb4b943
Shane <shanescott86@gmail.com>
2019-04-29 17:43:28 -0500

    eeee

--- Scripts/World/World Gold Stick.gd
+++ Scripts/World/World Gold Stick.gd
@@ -10,10 +10,10 @@ func _ready():
     camera = get_parent().get_node('Camera2D')
     player = get_parent().get_node('Player')
     podium = get_parent().get_node('stick_podium')
-	player.connect('gold_stick_picked_up', self, '_on_Player_gold_stick_picked_up')
+    player.connect('gold_stick_picked_up', self, '_on_Player_gold_stick_picked_up')
     self.connect('body_entered', player, '_on_World_Gold_Stick_body_entered')
     
-func _process():
+func _process(delta):
     camera.target_name = 'stick_podium'
     camera.zoom.x = podium.position.distance_to(player.position)
     camera.zoom.y = podium.position.distance_to(player.position)
