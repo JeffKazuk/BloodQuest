2de553600b84e18831cd27dea40f47872af124ed
Shane <shanescott86@gmail.com>
2019-04-29 07:24:17 -0500

    inventory system

---
+++ Scenes/UI/TextureRect.gd
@@ -0,0 +1,22 @@
+extends TextureRect
+
+var player
+var stick = load("res://Sprites/Stick.png")
+var sword = load("res://Sprites/sword.png")
+var fire = load("res://Sprites/Test fire.png")
+var dagger = load("res://Sprites/Dagger.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_parent().get_parent()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player != null:
+		if player.equipped == 'stick':
+			set_texture(stick)
+		if player.equipped == 'sword':
+			set_texture(sword)
+		if player.equipped == 'fire':
+			set_texture(fire)
+		if player.equipped == 'dagger':
+			set_texture(dagger)
